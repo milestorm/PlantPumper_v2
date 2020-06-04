fdaa453304ceb388dd1504562db67463b9eb6fd0
Caivas, David <David.Caivas@firma.seznam.cz>
2020-06-04 10:35:25 +0200

    pinout

---
+++ include/pinout.h
@@ -0,0 +1,11 @@
+#define RELAY1 2
+#define RELAY2 3
+
+#define RTC_RST 8
+#define RTC_DAT 7
+#define RTC_CLK 6
+
+// LCD is connected via SDA and SCL
+// Arduino Nano
+// SDA: A4
+// SCL: A5
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
+#include <customChars.h>
+#include <pinout.h>
+#include <LiquidCrystal_I2C.h>
+#include <Time.h>
+#include <DS1302RTC.h>
+#include <OneButton.h>
+#include <RotaryEncoder.h>
+
+
+// Set the LCD address to 0x27 in PCF8574 by NXP and Set to 0x3F in PCF8574A by Ti
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+// Set RTC module
+DS1302RTC rtc(RTC_RST, RTC_DAT, RTC_CLK);
+
+
+
+// ===============================================================
 
 void setup() {
-  // put your setup code here, to run once:
+    lcd.begin();
+    // lcd.createChar(0, customChar);
+    lcd.home();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
 }
