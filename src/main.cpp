54437f11a68aeb1384612b1e6033eae3bfc4fcab
Caivas, David <David.Caivas@firma.seznam.cz>
2020-06-04 10:47:37 +0200

    custom chars

--- src/main.cpp
+++ src/main.cpp
@@ -13,13 +13,31 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // Set RTC module
 DS1302RTC rtc(RTC_RST, RTC_DAT, RTC_CLK);
 
+void createCustomChars() {
+    lcd.createChar(0, chr_Clock);
+    lcd.createChar(1, chr_First);
+    lcd.createChar(2, chr_Second);
+    lcd.createChar(3, chr_Faucet);
+    lcd.createChar(4, chr_Calendar);
 
+    lcd.createChar(10, chr_M);
+    lcd.createChar(20, chr_T);
+    lcd.createChar(30, chr_W);
+    lcd.createChar(40, chr_F);
+    lcd.createChar(50, chr_S);
+
+    lcd.createChar(11, chr_M_inv);
+    lcd.createChar(21, chr_T_inv);
+    lcd.createChar(31, chr_W_inv);
+    lcd.createChar(41, chr_F_inv);
+    lcd.createChar(51, chr_S_inv);
+}
 
 // ===============================================================
 
 void setup() {
     lcd.begin();
-    // lcd.createChar(0, customChar);
+    createCustomChars();
     lcd.home();
 }
 
